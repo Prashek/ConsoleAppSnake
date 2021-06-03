f8cc76601f946aa0313c6c6a5af1527813ed888a
Prashek <natprashek@gamail.com>
2021-06-03 20:45:23 +0300

    Добавьте файлы проекта.

---
+++ ConsoleAppSnake/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpArsenManukjanSnake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(1, 3, '*');
+            p1.Draw();
+
+            Point p2 = new Point(4, 5, '#');
+            p2.Draw();
+
+            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
+            line.Draw();
+
+            Console.ReadLine();
+        }
+    }
+}
